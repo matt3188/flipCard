dc05e3ef787045ab8962d8153aa2d7330c746b6d
Matt Coleman <hello@matt-coleman.co.uk>
2015-08-19 12:57:01 +0100

    Added TODO list to JS

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,12 @@
 // jshint devel:true
 console.log('\'Allo \'Allo!');
+
+
+/**
+ * TODO - Start with the basics
+ *
+ * - Generate cards with symbols
+ * - Randomise positions of cards
+ * - Animation when clicked and only have a max of 2 flipped at one time
+ */
+
