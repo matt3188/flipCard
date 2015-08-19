fa9b0ed67c4f01e5ba95a3a92ed354e1fad45a64
Matt Coleman <hello@matt-coleman.co.uk>
2015-08-19 12:57:34 +0100

    Added shuffle function

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,4 +13,16 @@ console.log('\'Allo \'Allo!');
 $(function(){
 
   'use strict';
+
+  function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
 });
