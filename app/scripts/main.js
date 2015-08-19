5ee6d5c9ad3e74a0dabb9bf26aa6af285574e4fa
Matt Coleman <hello@matt-coleman.co.uk>
2015-08-19 14:50:37 +0100

    JS Formatting

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,11 +17,11 @@ $(function(){
   function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
     while (0 !== currentIndex) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
     }
     return array;
   }
