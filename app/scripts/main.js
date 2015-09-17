40a9e13e9a631931e9b373524168fe58876e932d
Matt Coleman <hello@matt-coleman.co.uk>
2015-09-17 14:29:34 +0100

    Added animationEvent function

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -169,6 +169,25 @@ $(function() {
   }
   var transitionEvent = whichTransitionEvent();
 
+  function whichAnimationEvent(){
+    var t,
+        el = document.createElement('fakeelement');
+
+    var animations = {
+      'animation'      : 'animationend',
+      'OAnimation'     : 'oAnimationEnd',
+      'MozAnimation'   : 'animationend',
+      'WebkitAnimation': 'webkitAnimationEnd'
+    };
+
+    for (t in animations){
+      if (el.style[t] !== undefined) {
+        return animations[t];
+      }
+    }
+  }
+  var animationEvent = whichAnimationEvent();
+
   /**
    * Key events
    */
