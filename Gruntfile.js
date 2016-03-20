46a5ae0c7146a642352a0946e5384b0ac2043c69
Matt Coleman <mattcoleman@matt-colemans-macbook-pro.local>
2016-03-20 21:27:40 +0000

    Updated image min contrib module and fixed imagemin src path

--- Gruntfile.js
+++ Gruntfile.js
@@ -247,7 +247,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= config.app %>/images',
-          src: '{,*/}*.{gif,jpeg,jpg,png}',
+          src: ['**/*.{gif,jpeg,jpg,png}'],
           dest: '<%= config.dist %>/images'
         }]
       }
